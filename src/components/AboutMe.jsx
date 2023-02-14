5a2420779034bfca075279ccb015d6b4431fb2cb
ivanosquis10 <83567373+ivanosquis10@users.noreply.github.com>
2023-02-14 12:47:38 -0800

    se agrego en la parte del aboutme el tamaño de las fuentes y se arreglo el contenedor del texto y ademas se agrego border a los botones con un shadow nuevo

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -8,33 +8,33 @@ const AboutMe = () => {
     >
       <div className="mx-auto max-w-full px-5 md:px-10">
         <div className="text-center">
-          <h2 className="mt-3 mb-3 text-[4rem] md:text-[5rem] font-medium leading-[4rem] tracking-tight text-slate-200">
+          <h1 className="mt-3 mb-3 text-[4rem] md:text-[5rem] font-medium leading-[4rem] tracking-tight text-slate-200">
             I'm
             <span className="text-green-500 font-bold"> Iván Rodríguez</span>
-          </h2>
+          </h1>
           <p className="text-2xl md:text-3xl font-bold leading-8 text-slate-300 mb-3">
             Frontend Developer React
           </p>
-          <div className="grid md:grid-cols-2 items-center md:gap-4">
-            <p className="mt-2 md:mb-2 text-base md:text-lg leading-relaxed text-slate-300 px-3 md:p-0">
+          <div className="md:w-10/12 mx-auto grid items-center gap-2">
+            <p className="mt-2 md:mb-2 text-base leading-relaxed text-slate-300 tracking-wide px-2">
               Tengo 22 años de edad y soy de Venezuela. Soy una persona muy
-              proactiva, con ganas de aprender y mejorar cada vez más
+              proactiva, con ganas de aprender y mejorar cada día más
             </p>
-            <p className="mt-2 mb-2 text-base md:text-lg leading-relaxed text-slate-300 px-3 md:p-0">
+            <p className="mt-2 mb-2 text-base leading-relaxed text-slate-300 tracking-wide px-1">
               Me considero un buen compañero de equipo, muy comunicativo,
               positivo, analista, apasionado, dedicado y responsable
             </p>
           </div>
         </div>
 
-        <div className="w-1/2 mx-auto mt-6 flex flex-col items-center justify-center md:flex-row gap-4 ">
+        <div className="w-full md:w-1/2 mx-auto mt-6 flex flex-col items-center justify-center md:flex-row gap-4 ">
           <Link
             to="projects"
             spy={true}
             smooth={true}
             offset={10}
             duration={500}
-            className="w-full uppercase transform rounded-md transparent shadow shadow-white px-5 py-3  font-bold text-white text-center transition-colors hover:bg-green-700 hover:cursor-pointer"
+            className="w-full uppercase transform rounded-md shadow-3xl border-2 border-green-700 px-5 py-3 font-bold text-white text-center ease-in-out duration-300 hover:border-green-500 hover:cursor-pointer"
           >
             My Projects
           </Link>
@@ -44,7 +44,7 @@ const AboutMe = () => {
             smooth={true}
             offset={10}
             duration={500}
-            className="w-full uppercase transform rounded-md transparent shadow shadow-white px-5 py-3  font-bold text-white text-center transition-colors hover:bg-green-700 hover:cursor-pointer"
+            className="w-full uppercase transform rounded-md shadow-3xl border-2 border-green-700 px-5 py-3 font-bold text-white text-center ease-in-out duration-300 hover:border-green-500 hover:cursor-pointer"
           >
             Contact me
           </Link>
